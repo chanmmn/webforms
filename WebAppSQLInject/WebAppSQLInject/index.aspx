4ac45462b0a9699416fe9fb153f19049780ad55d
chanmmn <33271651+chanmmn@users.noreply.github.com>
2022-05-28 23:25:36 +0800

    Commit WebAppSQLInject
    Commit WebAppSQLInject for SQL Server

---
+++ WebAppSQLInject/WebAppSQLInject/index.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="WebAppSQLInject.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblUser" runat="server" Text="Username"></asp:Label>
+            <br/>
+            <asp:TextBox ID="txtUser" runat="server"></asp:TextBox>
+            <br/>
+            <asp:Label ID="lblPassword" runat="server" Text="Password"></asp:Label>
+            <br/>
+            <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+            <br/>
+            <asp:Label ID="lblMsg" runat="server" Text=""></asp:Label>
+            <br/>
+            <asp:Button ID="cmdLogin" runat="server" Text="Button" OnClick="cmdLogin_Click" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebAppSQLInject/WebAppSQLInject/index.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppSQLInject
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void cmdLogin_Click(object sender, EventArgs e)
+        {
+            string strCnx = "server=localhost;database=northwind;uid=sa;pwd=Pa$$w0rd;";
+            SqlConnection cnx = new SqlConnection(strCnx);
+            cnx.Open();
+            //This code is susceptible to SQL injection attacks.
+            string strQry = "SELECT Count(*) FROM Users WHERE UserName='" + txtUser.Text + "' AND Password='" + txtPassword.Text + "'";
+            int intRecs;
+            SqlCommand cmd = new SqlCommand(strQry, cnx);
+            intRecs = (int)cmd.ExecuteScalar();
+            if (intRecs > 0)
+            {
+                //' Or 1=1 --
+                //FormsAuthentication.RedirectFromLoginPage(txtUser.Text, false);
+                lblMsg.Text = "Login attempt successful.";
+            }
+            else
+            {
+                lblMsg.Text = "Login attempt failed.";
+            }
+            cnx.Close();
+        }
+    }
+}
